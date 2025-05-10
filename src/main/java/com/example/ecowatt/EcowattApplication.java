5a95d86698c9ba042b0033451ab39cbc93e8af37
Victor Mazzola <victormazzola2019@gmail.com>
2025-05-10 10:43:41 -0300

    initial commit setup with project structure

---
+++ src/main/java/com/example/ecowatt/EcowattApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecowatt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcowattApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcowattApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecowatt/EcowattApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecowatt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcowattApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
